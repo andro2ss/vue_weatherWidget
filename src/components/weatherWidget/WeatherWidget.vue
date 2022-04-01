369d5b410fe0dc041019c467dcbd5229d1249f8c
andro2ss <a.brzeski@hotmail.com>
2022-04-01 11:21:43 +0200

    created InputBox component without style

--- src/components/weatherWidget/WeatherWidget.vue
+++ src/components/weatherWidget/WeatherWidget.vue
@@ -1,12 +1,15 @@
 <template>
 <div class="weatherWidget__container">
   <h1>Open Weather</h1>
+  <InputBox />
 </div>
 </template>
 
 <script>
+import InputBox from "@/components/weatherWidget/items/InputBox";
 export default {
   name: "WeatherWidget",
+  components: {InputBox},
   data() {
     return {
       weatherData: null,
---
+++ src/components/weatherWidget/items/InputBox.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="inputBox__container">
+    <h2 class="inputBox__title">Podaj lokalizację</h2>
+    <div class="inputBox_boxes">
+      <div class="box__input">
+        <input type="text" v-bind:class="(!userSearchError)?'box__input':'box__input input__error'"
+               v-model="inputValue" placeholder="Podaj lokalizację"
+               v-on:keyup.enter="handleUserSearchValue(inputValue)"/>
+        <span v-if="userSearchError" class="error__message">Nie znaleziono</span>
+      </div>
+      <button class="box__btn" @click="handleUserSearchValue(inputValue)">Wyszukaj</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputBox",
+  data() {
+    return {
+      inputValue: '',
+    };
+  },
+  props: ['userSearchValue', 'userSearchError', 'gettingData'],
+  methods: {
+    handleUserSearchValue(searchValue) {
+      this.$emit('handle-UserSearchValue', searchValue);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
