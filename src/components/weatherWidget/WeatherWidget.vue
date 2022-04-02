7455193c016317ef79a380887203a9e0b3ff8822
andro2ss <a.brzeski@hotmail.com>
2022-04-02 08:54:39 +0200

    created function to get weather data by city

--- src/components/weatherWidget/WeatherWidget.vue
+++ src/components/weatherWidget/WeatherWidget.vue
@@ -8,6 +8,7 @@
 
 <script>
 import InputBox from "@/components/weatherWidget/items/InputBox";
+import {loadDataCity} from "@/components/weatherWidget/helpers/loadDataCity";
 
 export default {
   name: "WeatherWidget",
@@ -22,9 +23,16 @@ export default {
       gettingData: false,
     }
   },
+  created() {
+
+  },
   methods: {
     handleUserSearchValue(searchValue) {
       this.userSearchValue = searchValue;
+      if (!this.gettingData) {
+        this.gettingData = true;
+        loadDataCity.call(this, searchValue);
+      }
     },
   },
 }
